515bb5c3e8c907699eda1efe07f84d6251b2eb2e
v-king777 <v.kolpashchikov@gmail.com>
2021-01-12 20:30:03 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BullsAndCows
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите 4-х значное целое число: ");
+            string strRaw = Console.ReadLine();
+            string str = strRaw.Replace(" ", "");
+            bool checkLenght = str.Length == 4;
+            ushort num;
+            bool checkNum = UInt16.TryParse(str, out num);
+            Console.WriteLine("Проверка на длину - {0}", checkLenght);
+            Console.WriteLine("Проверка на число - {0}", checkNum);
+            Console.WriteLine("Число = {0}", num);
+        }
+    }
+}
